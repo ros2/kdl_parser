bc1572e15c0d8dd5877fba0bf909ccd22dfa4adf
Dan Rose <dan@digilabs.io>
2020-05-14 16:03:02 -0500

    stop using treeFromXml internally

--- kdl_parser/src/kdl_parser.cpp
+++ kdl_parser/src/kdl_parser.cpp
@@ -160,9 +160,11 @@ bool addChildrenToTree(urdf::LinkConstSharedPtr root, KDL::Tree & tree)
 
 bool treeFromFile(const std::string & file, KDL::Tree & tree)
 {
-  TiXmlDocument urdf_xml;
-  urdf_xml.LoadFile(file);
-  return treeFromXml(&urdf_xml, tree);
+  std::ifstream t(file);
+  std::stringstream buffer;
+  buffer << t.rdbuf();
+
+  return treeFromString(buffer.str(), tree);
 }
 
 bool treeFromParam(const std::string & param, KDL::Tree & tree)
@@ -181,9 +183,12 @@ bool treeFromParam(const std::string & param, KDL::Tree & tree)
 
 bool treeFromString(const std::string & xml, KDL::Tree & tree)
 {
-  TiXmlDocument urdf_xml;
-  urdf_xml.Parse(xml.c_str());
-  return treeFromXml(&urdf_xml, tree);
+  urdf::Model robot_model;
+  if (!robot_model.initString(xml.c_str())) {
+    fprintf(stderr, "Could not generate robot model\n");
+    return false;
+  }
+  return treeFromUrdfModel(robot_model, tree);
 }
 
 bool treeFromXml(TiXmlDocument * xml_doc, KDL::Tree & tree)
